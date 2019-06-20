6c23c60a111e77ee470169d291b2a2f11f2fc07b
Aung Khant <aungkhant.imh@gmail.com>
2019-06-20 14:31:09 +0630

    Updates

---
+++ precache-manifest.5a5dca234e9ce18b71e273e54436db2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6512c1f94481a339c113b0f93e7903e",
+    "url": "/lyricsfinder/index.html"
+  },
+  {
+    "revision": "85f9d0beca47d61c7c91",
+    "url": "/lyricsfinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a5d8064f9e7986e2da70",
+    "url": "/lyricsfinder/static/js/2.b53601b4.chunk.js"
+  },
+  {
+    "revision": "85f9d0beca47d61c7c91",
+    "url": "/lyricsfinder/static/js/main.accea72d.chunk.js"
+  },
+  {
+    "revision": "477a4ea197ae4bf199ab",
+    "url": "/lyricsfinder/static/js/runtime~main.a9ea7de9.js"
+  }
+]);
